b56f6081141864e5d03b0ae815bde29b98252cdf
igniy <kvashnin.iura@yandex.ru>
2024-05-22 01:44:46 +0300

    change api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://freelanceapp-back.onrender.com:10000/api',
+    baseURL: 'https://freelanceapp-back.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
